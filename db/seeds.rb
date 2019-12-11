6d6a78da6721913c76565c03c74bbc5bf9f6f4b3
grewa1 <gbjrmail@gmail.com>
2019-12-11 11:13:01 -0500

    adjusted seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,13 +23,13 @@ arts = Art.create([{
   art_name: 'Basquiat',
   url: "https://i.pinimg.com/736x/21/90/29/2190291f8069fd8ada9aa6bd6d084331.jpg",
   description: "This is a quick mural with Basquiat",
-  user_id: users.first
+  user_id: 1
   }, 
   { username: 'David Bowe',
     art_name: 'Unknown',
     url: "https://i.pinimg.com/originals/56/b7/a4/56b7a425ef719a132680f832885cd669.jpg",
     description: "An unknown artist with pictures looking up.",
-    user_id: users.second
+    user_id: 2
   }])
 
 
@@ -38,12 +38,12 @@ arts = Art.create([{
     username: 'Basquiat',
     art_name: 'Unknown',
     description: "An unknown artist with pictures looking up.",
-   user_id: users.first,
+   user_id: 1,
    art_id: arts.second
 },
   {username: 'David Bowe',
   art_name: 'Unknown',
   description: "An unknown artist with pictures looking up.",
-  user_id: users.second,
+  user_id: 2,
   art_id: arts.first
   }])
